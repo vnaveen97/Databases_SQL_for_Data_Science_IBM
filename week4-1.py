c455b9e05198c42da74d99b78c7f83709cc401c0
Naveen <vnaveen2797@gmail.com>
2020-02-09 18:56:39 -0600

    Update week4-1.py

--- week4-1.py
+++ week4-1.py
@@ -45,24 +45,6 @@ get_ipython().run_line_magic('load_ext', 'sql')
 
 # In[ ]:
 
-
-{
-  "db": "BLUDB",
-  "dsn": "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50000;PROTOCOL=TCPIP;UID=jqv77754;PWD=hcg@sfkcm5v1dzs5;",
-  "host": "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
-  "hostname": "dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
-  "https_url": "https://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net",
-  "jdbcurl": "jdbc:db2://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50000/BLUDB",
-  "parameters": {},
-  "password": "hcg@sfkcm5v1dzs5",
-  "port": 50000,
-  "ssldsn": "DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;PORT=50001;PROTOCOL=TCPIP;UID=jqv77754;PWD=hcg@sfkcm5v1dzs5;Security=SSL;",
-  "ssljdbcurl": "jdbc:db2://dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50001/BLUDB:sslConnection=true;",
-  "uri": "db2://jqv77754:hcg%40sfkcm5v1dzs5@dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net:50000/BLUDB",
-  "username": "jqv77754"
-}
-
-
 # In[3]:
 
 
